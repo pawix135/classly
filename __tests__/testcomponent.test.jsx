971e29fecc15fe07a355041ca791f37ff7a83edb
pawix135 <leagueoflegendsoppl@gmail.com>
2023-12-20 08:50:55 +0100

    add second test

--- __tests__/testcomponent.test.jsx
+++ __tests__/testcomponent.test.jsx
@@ -1,12 +1,23 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import TestComponent from "@/components/TestComponent";
+import TestComponentTwo from "@/components/TestComponentTwo";
 
 describe("Test Component", () => {
   it("renders a heading", () => {
     render(<TestComponent />);
 
     const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
+
+describe("Test Component Two", () => {
+  it("renders a heading", () => {
+    render(<TestComponent />);
+
+    const heading = screen.getByRole("p", { level: 1 });
 
     expect(heading).toBeInTheDocument();
   });
---
+++ src/components/TestComponentTwo/index.tsx
@@ -0,0 +1,9 @@
+const TestComponentTwo: React.FC = () => {
+  return (
+    <div>
+      <p>This shall not pass</p>
+    </div>
+  );
+};
+
+export default TestComponentTwo;
